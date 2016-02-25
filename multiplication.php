0a85b6ce7e5790c7bc6a72fcaf3d09dd8dd5117a
okaysierra <scseacat@gmail.com>
2016-02-25 10:19:40 -0600

    Add initial row and column of numbers

--- multiplication.php
+++ multiplication.php
@@ -5,9 +5,20 @@
   }
 
   echo "<table style=width:100%>";
+  echo "<tr>";
+  echo "<td> </td>";
+
+  for($k=1; $k <=100; $k++)
+  {
+    echo "<td>" . $k . "</td>";
+  }
+
+  echo "</tr>";
+
   for($i=1; $i <= 100; $i++)
   {
     echo "<tr>";
+    echo "<td>" . $i . "</td>";
     for($j=1; $j <=100; $j++)
     {
       echo "<td>" . multiply($i,$j) . "</td>";
