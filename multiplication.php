51fb4d8e8448bff040a3f5a8bc2e42582af393e0
okaysierra <scseacat@gmail.com>
2016-02-25 09:50:35 -0600

    Write multiplication table function

---
+++ multiplication.php
@@ -0,0 +1,16 @@
+<?php
+  function multiply($x, $y) {
+    $z = $x*$y;
+    return $z;
+  }
+
+  for($i=1; $i <= 100; $i++)
+  {
+    echo "<tr>";
+    for($j=1; $j <=100; $j++)
+    {
+      echo "<td>" . multiply($i,$j) . "</td>";
+    }
+    echo "</tr>"
+  }
+?>
