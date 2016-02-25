5b55688cba3b91a2ca30b6d8ec46062aa1c2a7e6
Sierra Seacat <s831s804@ku.edu>
2016-02-25 13:39:51 -0600

    add answer output

--- quiz.php
+++ quiz.php
@@ -1,1 +1,31 @@
 <?php
+
+$A1 = $_POST["Q1"];
+$A2 = $_POST["Q2"];
+$A3 = $_POST["Q3"];
+$A4 = $_POST["Q4"];
+$A5 = $_POST["Q5"];
+
+echo "Question 1: What is the capital city of Portugal? <br>";
+echo "You answered" .$A1 . "<br>";
+echo "Correct answer: a) Lisbon <br><br>";
+
+echo "Question 2: What is the capital city of Germany? <br>";
+echo "You answered:" . $A2 . "<br>";
+echo "Correct answer: c) Berlin <br><br>";
+
+echo "Question 3: What is the capital city of the United Kingdom?<br>";
+echo "You answered:" . $A3 . "<br>";
+echo "Correct answer: b) London <br><br>";
+
+echo "Question 4: What is the capital city of France?<br>";
+echo "You answered:" . $A4 . "<br>";
+echo "Correct answer: a) Paris<br><br>";
+
+echo "Question 5: What is the capital city of Russia?<br>";
+echo "You answered:" . $A5 . "<br>";
+echo "Correct answer: d) Moscow<br><br>";
+
+
+
+?>
