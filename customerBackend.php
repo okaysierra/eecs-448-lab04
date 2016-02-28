d2892a5e021233601b9d2585fda2744cae370c53
okaysierra <scseacat@gmail.com>
2016-02-27 18:00:47 -0600

    TRY TO GET FORMCHECKER.JS TO WORK

--- customerBackend.php
+++ customerBackend.php
@@ -1,7 +1,7 @@
 <?php
 
   $email = $_POST["email"];
-  $password = $_POST["Password"];
+  $password = $_POST["password"];
   $plates = $_POST["plates"];
   $pillow = $_POST["pillow"];
   $tote = $_POST["tote"];
@@ -9,20 +9,21 @@
   echo "<h2> Welcome " .$email. ".<br>Your password is: " . $password . "</h2>";
 
   function cost($A, $B, $C, $D) {
-    $cost = ($A*25.00)+($B*15)+($C*10)+$D;
+    $cost = ($A*25.00)+($B*15.00)+($C*10.00)+$D;
     return $cost;
   }
 
   function shipping($E){
-    if($E === 0.00)
+    $shipOpt=null;
+    if($E == "0")
     {
       $shipOpt = "Free";
     }
-    else if($E ===5.00)
+    else if($E == "5")
     {
       $shipOpt = "Three day";
     }
-    else if($E = 50.00)
+    else if($E == "50")
     {
       $shipOpt = "Overnight";
     }
@@ -31,11 +32,11 @@
 
   echo "<div id = receipt>";
   echo "You ordered:<br>";
-  echo "Plates with handpainted bird designs (set of four) x" . $plates . "-------> $". $plates*25 . "<br>";
-  echo "Throw pillow with embossed bird design x" . $pillow . "-------------------> $". $pillow*15 . "<br>";
-  echo "Tote bag with bird design x" . $tote . "----------------------------------> $". $tote*10 . "<br>";
-  echo "Shipping type" .shipping($shipping) . "-----------------------------------> $". $shipping . "<br>";
-  echo "total --------------------------------------------------------------------> $". cost($plates, $pillow, $tote, $shipping) . "<br>";
+  echo "Plates with handpainted bird designs (set of four) x" . $plates . " -------------- $". $plates*25.00 . "<br>";
+  echo "Throw pillow with embossed bird design x" . $pillow . " ------------------------ $". $pillow*15.00 . "<br>";
+  echo "Tote bag with bird design x" . $tote . " ------------------------------------------- $". $tote*10.00 . "<br>";
+  echo "Shipping type: " .shipping($shipping) . " ------------------------------------------- $". $shipping . "<br>";
+  echo "<b>total ------------------------------------------------------------------------ $". cost($plates, $pillow, $tote, $shipping) . "</b><br>";
   echo "</div>";
 
 ?>
--- formChecker.js
+++ formChecker.js
@@ -1,33 +1,33 @@
 function validate() {
-  var x = document.forms["order"]["email"].value;
-  var y = document.forms["order"]["password"].value;
-  var z = document.forms["order"]["plates"].value;
-  var a = document.forms["order"]["pillow"].value;
-  var b = document.forms["order"]["tote"].value;
-  var c = document.getElementsByName("shipping").value;
+  var x = document.getElementById("email").value;
+  var y = document.getElementById("password").value;
+  var z = document.getElementById("plates").value;
+  var a = document.getElementById("pillow").value;
+  var b = document.getElementById("tote").value;
+  var c = document.getElementsByTagName("shipping").value;
 
   if(x==null || x==""){
-    alert("Email must be filled out");
+    window.alert("Email must be filled out");
     return false;
   }
   else if(y==null || y=="") {
-    alert("You must enter a password");
+    window.alert("You must enter a password");
     return false;
   }
   else if(z==null || z=="" || a==null || a=="" || b==null || b==""){
-      alert("Quantities cannot be blank");
+      window.alert("Quantities cannot be blank");
       return false;
   }
   else if(z<0 || a<0 || b<0){
-    alert("Quantities must be greater than or equal to zero");
+    window.alert("Quantities must be greater than or equal to zero");
     return false;
   }
   else if(c==null){
-    alert("You must select a shipping option");
+    window.alert("You must select a shipping option");
     return false;
   }
   else if(emailcheck(x)!=true){
-    alert("Please enter a valid email address")
+    window.alert("Please enter a valid email address")
     return false;
   }
   else{
